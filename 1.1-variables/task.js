dc7b6bdc1b975ae99058c19ddf8f7d0c1f392707
Vera Timoshina <lionfishka@gmail.com>
2020-04-24 12:27:28 +0300

    Homework 1.1.-variables completed

--- 1.1-variables/task.js
+++ 1.1-variables/task.js
@@ -7,21 +7,30 @@
 
 function averageMark(a, g, p) {
 	
-    // реализуйте здесь задание №1
+	let algebra = a;
+	let georgaphy = g;
+	let physics = p;
+
+	let average = (algebra + georgaphy + physics) / arguments.length;
     
-	return // ваш ответ;
+	return average; 
 }
 
 function sayHello(userName) {
 	
-    // реализуйте здесь задание №2
+	let myName = userName;
+	let message = `Привет, мир! Меня зовут ${myName}`;
     
-	return // ваш ответ;
+	return message; 
 }
 
 function calculateFormula() {
     
-	// реализуйте здесь задание №3
+	let x = 2; 
+	let y = 22;
+	let z = 0;
+	
+	let result = x * y + 5 * z + x - 1;
     
-    return // ваш ответ;
+    return result; 
 }
