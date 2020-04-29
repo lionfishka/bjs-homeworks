b37c01dccf938145b0cd9b8ae3e8253d6aa9536c
Vera Timoshina <lionfishka@gmail.com>
2020-04-29 13:54:38 +0300

    Second commit

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -4,14 +4,10 @@ function getResult(a,b,c){
     let x = [];
 
     if (D > 0) {
-        x = [];
         x[0] = (-b + Math.sqrt(D)) / (2*a);
         x[1] = (-b - Math.sqrt(D)) / (2*a);
     } else if (D === 0) {
-        x = [];
         x[0] = (-b + Math.sqrt(D)) / (2*a);
-    } else if (D < 0) {
-        x = [];
     }
 
     return x;
@@ -19,33 +15,30 @@ function getResult(a,b,c){
 
 
 function getAverageMark(marks){
-    let sum = 0;
-    let averageMark = 0;
 
+    if (!marks.length) {
+        return 0;
+    } 
+
+    if (marks.length > 5) {
+        console.log('Количество оценок больше 5');
+        marks = marks.slice(0,5);
+    }
+   
+    let sum = 0;
     for (let i = 0; i <= marks.length; i++) {
         sum += marks[i];
-        averageMark = sum / marks.length;
-
-        if (!marks.length) {
-            return 0;
-        } else if (marks.length > 5) {
-            console.log("Количество оценок больше 5");
-            marks = marks.slice(0,5);
-            sum += marks;
-            averageMark = sum / 5;
-        }
     }
-
+  
+    let averageMark = sum / marks.length;
     return averageMark;
 }
 
 
 function askDrink(name,dateOfBirthday){
-    let year = new Date().getFullYear();
-    let age = year - dateOfBirthday.getFullYear();
-    if (age >= 18) {
+    if (new Date().getFullYear() - dateOfBirthday.getFullYear() >= 18) {
         result = `Не желаете ли олд-фэшн, ${name}?`;
-    } else if (age < 18) {
+    } else if (new Date().getFullYear() - dateOfBirthday.getFullYear() < 18) {
         result = `Сожалею, ${name}, но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!`;
     } 
 
