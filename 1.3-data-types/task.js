6866ecd3e103ba0728de216dbf98d5db54f0f7df
Vera Timoshina <lionfishka@gmail.com>
2020-05-03 11:29:24 +0300

    First commit

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,9 +1,29 @@
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    // код для задачи №1 писать здесь
-    // return totalAmount;
+    "use strict";
+
+    let numPercent = new Number(percent);
+    let numContribution = new Number(contribution);
+    let numAmount = new Number(amount);
+
+    if (isNaN(numPercent)) {return `Параметр ${percent} содержит неправильное значение`;}
+    if (isNaN(numContribution)) {return `Параметр ${contribution} содержит неправильное значение`;}
+    if (isNaN(numAmount)) {return `Параметр ${amount} содержит неправильное значение`;}
+
+    let s = numAmount - numContribution;
+    let n = (date.getFullYear() - new Date().getFullYear()) * 12;
+    let p = numPercent / 12;
+    let monthlyPayment = s*(p+p/(((1+p)**n)-1));
+    let totalAmount = parseInt((monthlyPayment * n).toFixed(2));
+
+    return totalAmount;
 }
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    // return greeting;
+
+    if(!name) {
+        name = "Аноним";
+    }
+    let greeting = `Привет, мир! Меня зовут ${name}`;
+
+    return greeting;
 }
