7a9a2d2fdd563ca74ebb4a2022575c21cd440093
Vera Timoshina <lionfishka@gmail.com>
2020-05-12 17:26:37 +0300

    Initial commit

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,14 +1,35 @@
 function getAnimalSound(animal) {
-    // код для задачи №1 писать здесь
-    // return animalSound;
+    const animalSound = animal.sound;
+    if (animal === undefined) {
+        return null;
+    } else {  
+        return animalSound;
+    }
 }
 
-function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
-    // return averageMark
+function getAverageMark(...marks) {
+    const sum = 0;
+    for (let i = 0; i < marks.length; i++) {
+        sum += marks[i];
+    }
+    const average = sum / marks.length;
+    return Math.round(average);
 }
 
 function checkBirthday(birthday) {
-    // код для задачи №3 писать здесь
-    // return verdict
+    
+    const now = Date.now();
+    new Date (1900, 0, 1);
+    const birthday = new Date.now();
+    const diff = now - birthday;
+    const age = diff / date.Milliseconds;
+    const verdict;
+    
+    if (age > 18) {
+        verdict = "Да";
+    } else {
+        verdict = "Нет";
+    }
+
+return verdict;
 }
