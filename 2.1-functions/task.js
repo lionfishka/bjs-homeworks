a0e493e468e93ff35b9a68cab6fdcab1173236ee
Vera Timoshina <lionfishka@gmail.com>
2020-05-19 15:38:27 +0300

    2.1 functions second commit

--- 2.1-functions/task.js
+++ 2.1-functions/task.js
@@ -1,47 +1,73 @@
 function getSolutions(a,b,c) {
 
     let D = (b**2) - (4*a*c);
-    let roots;
+    // let roots = [];
+    let x1;
+    let x2;
 
     if (D < 0) {
-        return `D: ${D}, roots: ${roots}`;
+        return D;
     } else if (D === 0) {
         x1 = (-b + Math.sqrt(D)) / (2*a);
-        return `D: ${D}, roots: ${x1}`;
+        // return [D, roots];
+        return [D, x1];
     } else if (D > 0) {
         x1 = (-b + Math.sqrt(D)) / (2*a);
         x2 = (-b - Math.sqrt(D)) / (2*a);
-        return `D: ${D}, roots: ${x1}, ${x2}`;
+        return [D, x1, x2];
     }
 
-    return x;
+    // return x;
 }
 
 function showSolutionsMessage(a, b, c) {
     let result = getSolutions(a,b,c);
+    let D = result.D;
     console.log(`Вычисляем корни квадратного уравнения ${a}x² + ${b}x + ${c}`);
-    console.log(`Значение дискриминанта: ${D}`);
-    if (result === []) {
+    console.log(`Значение дискриминанта: ${result.D}`);
+    if (D < 0) {
         console.log(`Уравнение не имеет вещественных корней`);
-    } else if (result === [x1]) {
-        console.log(`Уравнение имеет один корень X₁ = ${x1}`);
+    } else if (D === 0) {
+        console.log(`Уравнение имеет один корень X₁ = ${result.x1}`);
     } else {
-        console.log(`Уравнение имеет два корня. X₁ = ${x1}, X₂ = ${x2}`);
+        console.log(`Уравнение имеет два корня. X₁ = ${result.x1}, X₂ = ${result.x2}`);
     }
 }
 
 function getAverageScore(data) {
-    let data = {
+    const inputData = {
         subj: [algebra, geometry, russian, physics, music, english, poetry, chemistry, french],
         marks: [],
     };
+
+    for (marks in inputData) {
+        return [`algebra: ${average}, \n
+        geometry: ${average}, \n
+        russian: ${average}, \n
+        physics: ${average}, \n
+        music: ${average}, \n
+        english: ${average}, \n
+        poetry: ${average}, \n
+        chemistry: ${average}, \n
+        french: ${average}, \n
+        average: ${average}`];
+    }        
 }
 
 function getAverageMark(marks){
 
-    for(subj in data) {
-        marks += getAverageMark(data[subj]);
-    }
+    let sum = 0;
+    let average;
+    for(subj in inputData) {
+        if (inputData.marks.length === 0) {
+         return 0;
+            } else {
+                 for (let i = 0; i < inputData.marks.length; i++) {
+                sum += Number(inputData.marks[i]);
+            }
 
-    console.log({subj, marks});
+         average = sum / inputData.marks.length;
+    }
+    // console.log({subj, marks});
+    }
 }
