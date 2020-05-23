baf7aca127b78f26d254959061f6545b5a27d6e4
Vera Timoshina <lionfishka@gmail.com>
2020-05-23 13:09:37 +0300

    2.3-oop-es6 new commit

--- 2.3-oop-es6/task.js
+++ 2.3-oop-es6/task.js
@@ -1,19 +1,23 @@
 class Weapon {
-    constructor() {
-        this.name = name;
-        this.attack = attack;
-        this.durability = durability;
-        this.range = range;
+    constructor(obj) {
+        this.name = obj.name;
+        this.attack = obj.attack;
+        this.durability = obj.durability;
+        this.range = obj.range;
+        this.startDurability = obj.durability;
     }
 
     takeDamage(damage) {
-        durability = this.durability - damage;
+        this.durability -= damage;
+        if (this.durability < 0) {
+            this.durability = 0;
+        }
     }
 
     getDamage() {
-        if (durability===0) {
+        if (this.durability===0) {
             return 0;
-        } else if (durability >= (this.durability*0,3)) {
+        } else if (this.durability >= (this.startDurability*0,3)) {
             return this.attack;
         } else {
             return this.attack / 2;
@@ -21,7 +25,7 @@ class Weapon {
     }
 
     isBroken() {
-        if (durability===0) {
+        if (this.durability===0) {
             return true;
         } else {
             return false;
@@ -40,73 +44,149 @@ const stormStaff = new Weapon('Посох Бури', 10, 300, 3);
 
 
 class Arm extends Weapon {
-    constructor() {
-        this.name = 'Рука';
-        this.attack = 1;
-        this.durability = Infinity;
-        this.range = 1;
+    constructor(obj) {
+        super(Object.assign({
+            name = 'Рука',
+            attack = 1,
+            durability = Infinity,
+            range = 1,
+        }, obj));  
     }
 }
 
 class Bow extends Weapon {
-    constructor() {
-        this.name = 'Лук';
-        this.attack = 10;
-        this.durability = 200;
-        this.range = 3;
+    constructor(obj) {
+        super(Object.assign({
+            name = 'Лук',
+            attack = 10,
+            durability = 200,
+            range = 3,
+        }, obj));  
     }
 }
 
 class Sword extends Weapon {
-    constructor() {
-        this.name = 'Меч';
-        this.attack = 25;
-        this.durability = 500;
-        this.range = 1;
+    constructor(obj) {
+        super(Object.assign({
+            name = 'Меч',
+            attack = 25,
+            durability = 500,
+            range = 1,
+        }, obj));  
     }
 }
 
 class Knife extends Weapon {
-    constructor() {
-        this.name = 'Нож';
-        this.attack = 5;
-        this.durability = 300;
-        this.range = 1;
+    constructor(obj) {
+        super(Object.assign({
+            name = 'Нож',
+            attack = 5,
+            durability = 300,
+            range = 1,
+        }, obj));  
     }
 }
 
 class Staff extends Weapon {
-    constructor() {
-        this.name = 'Посох';
-        this.attack = 8;
-        this.durability = 300;
-        this.range = 2;
+    constructor(obj) {
+        super(Object.assign({
+            name = 'Посох',
+            attack = 8,
+            durability = 300,
+            range = 2,
+        }, obj));  
     }
 }
 
 class LongBow extends Bow {
-    constructor() {
-        super(durability);
-        this.name = 'Длинный лук';
-        this.attack = 15;
-        this.range = 4;
+    constructor(obj) {
+        super(Object.assign({
+            name = 'Длинный лук',
+            attack = 15,
+            range = 4,
+        }, obj));  
     }
 }
 
 class Axe extends Sword {
-    constructor() {
-        super(range);
-        this.name = 'Секира';
-        this.attack = 27;
-        this.durability = 800;
+    constructor(obj) {
+        super(Object.assign({
+            name = 'Секира',
+            attack = 27,
+            durability = 800,
+        }, obj));  
     }
 }
 
 class StormStaff extends Staff {
-    constructor() {
-        super(durability);
-        this.name = 'Посох Бури';
-        this.attack = 10;
-        this.range = 3;
+    constructor(obj) {
+        super(Object.assign({
+            name = 'Посох Бури',
+            attack = 10,
+            range = 3,
+        }, obj));  
     }
 }
+
+// class Bow extends Weapon {
+//     constructor(obj) {
+//         this.name = 'Лук';
+//         this.attack = 10;
+//         this.durability = 200;
+//         this.range = 3;
+//     }
+// }
+
+// class Sword extends Weapon {
+//     constructor(obj) {
+//         this.name = 'Меч';
+//         this.attack = 25;
+//         this.durability = 500;
+//         this.range = 1;
+//     }
+// }
+
+// class Knife extends Weapon {
+//     constructor(obj) {
+//         this.name = 'Нож';
+//         this.attack = 5;
+//         this.durability = 300;
+//         this.range = 1;
+//     }
+// }
+
+// class Staff extends Weapon {
+//     constructor(obj) {
+//         this.name = 'Посох';
+//         this.attack = 8;
+//         this.durability = 300;
+//         this.range = 2;
+//     }
+// }
+
+// class LongBow extends Bow {
+//     constructor(obj) {
+//         super(durability);
+//         this.name = 'Длинный лук';
+//         this.attack = 15;
+//         this.range = 4;
+//     }
+// }
+
+// class Axe extends Sword {
+//     constructor(obj) {
+//         super(range);
+//         this.name = 'Секира';
+//         this.attack = 27;
+//         this.durability = 800;
+//     }
+// }
+
+// class StormStaff extends Staff {
+//     constructor(obj) {
+//         super(durability);
+//         this.name = 'Посох Бури';
+//         this.attack = 10;
+//         this.range = 3;
+//     }
+// }
