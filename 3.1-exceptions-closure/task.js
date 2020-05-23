4c8ec54328700863de567f852b3fcacb1b506352
Vera Timoshina <lionfishka@gmail.com>
2020-05-23 15:18:33 +0300

    3.1-exceptions-closure 1st commit

---
+++ 3.1-exceptions-closure/task.js
@@ -0,0 +1,57 @@
+//Задача №1. Форматтер чисел
+
+function parseCount(value) {
+    const newValue = Number.parseInt(value);
+    if (newValue == NaN) {
+        const parseCountError = new Error("Невалидное значение");
+        throw parseCountError;
+    } else {
+        return newValue;
+    }
+}
+
+function validateCount(value) {
+    try {
+        return parseCount(value);
+    } catch(e) {
+        return e;
+    }
+}
+
+//Задача №2. Треугольник
+
+class Triangle {
+    constructor(a,b,c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    // (сумма двух сторон меньше третьей) 
+    if () {
+        const triangleError = new Error("Треугольник с такими сторонами не существует");
+        throw triangleError;
+    }
+
+    getPerimeter() {
+        perimeter = a + b + c;
+        return perimeter;
+    }
+
+    getArea() {
+        let p = perimeter / 2;
+        area = Math.sqrt(p * (p - a) * (p - b) * (p - c))
+        return area.toFixed(3);
+    }
+}
+
+function getTriange(a,b,c) {
+    try {
+        return new Triangle(a,b,c);
+    } catch(e) {
+        return {
+            getArea: () => "Ошибка! Неправильный треугольник",
+            getPerimeter: () => "Ошибка! Неправильный треугольник"
+        }
+    }
+}
